f3022093876c3a93790b82f82e7b4e008f28e944
mwlai288 <mwlai288@gmail.com>
2018-09-05 14:26:42 -0400

    edited routes

--- 4week/redux-sass/src/environment.ts
+++ 4week/redux-sass/src/environment.ts
@@ -1,11 +1,9 @@
 const dev = {
-  context: 'http://localhost:3000/'
-}
+  context: "http://localhost:3000/"
+};
 
 const prod = {
-  context: 'http://ec2-35-164-166-180.us-west-2.compute.amazonaws.com:3000/'
-}
+  context: "http://ec2-18-218-165-41.us-east-2.compute.amazonaws.com:8080/"
+};
 
-export const environment = process.env.NODE_ENV === 'production'
-  ? prod
-  : dev
+export const environment = process.env.NODE_ENV === "production" ? prod : dev;
